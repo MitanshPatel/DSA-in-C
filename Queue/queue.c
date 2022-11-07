14c2125df4df9928dbf1101fff930dc55a78abe0
Mitansh Patel <82700614+MitanshPatel@users.noreply.github.com>
2022-11-07 23:04:50 +0530

    Update queue.c

--- Queue/queue.c
+++ Queue/queue.c
@@ -11,16 +11,16 @@ struct Q{
 
 typedef struct Q Q;
 
-Q *createQueue (int n)
-{
-    Q *queue;
-    queue=(Q*)malloc(sizeof(Q));
-    queue -> front=-1;
-    queue -> size=n;
-    queue -> rear=-1;
-    queue ->array=(int*)malloc(n*sizeof(int));
-    return queue;
-};
+// Q *createQueue (int n)
+// {
+//     Q *queue;
+//     queue=(Q*)malloc(sizeof(Q));
+//     queue -> front=-1;
+//     queue -> size=n;
+//     queue -> rear=-1;
+//     queue ->array=(int*)malloc(n*sizeof(int));
+//     return queue;
+// };
 
 int isFull(Q *queue){
     return (queue->rear==queue->size-1);
@@ -87,14 +87,18 @@ void displayQueue(Q *queue){
 }
 
 int main(){
-    Q *queue=createQueue(4);
-    enqueue(queue,45);
-    enqueue(queue,72);
-    enqueue(queue,87);
-    enqueue(queue,23);
-    displayQueue(queue);
-    dequeue(queue);
-    dequeue(queue);
-    dequeue(queue);
-    displayQueue(queue);
+    struct Q queue;
+    queue.size = 4;
+    queue.front = queue.rear = -1;
+    queue.array = (int*) malloc(queue.size*sizeof(int));
+    //Q *queue=createQueue(4);
+    enqueue(&queue,45);
+    enqueue(&queue,72);
+    enqueue(&queue,87);
+    enqueue(&queue,23);
+    displayQueue(&queue);
+    dequeue(&queue);
+    dequeue(&queue);
+    dequeue(&queue);
+    displayQueue(&queue);
 }
